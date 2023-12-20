1a42453532f47579be9a60931253fd02cb78f1df
Sibajene <sikassiba@gmail.com>
2023-12-20 21:28:12 +0200

    Update storage.js

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,8 +1,12 @@
+// services/storage.js
+
 export const saveChild = (child) => {
-    // Save child data to local storage
-  };
-  
-  export const getChildren = () => {
-    // Retrieve child data from local storage
-  };
-  
+  const existingChildren = JSON.parse(localStorage.getItem('children')) || [];
+  const updatedChildren = [...existingChildren, child];
+  localStorage.setItem('children', JSON.stringify(updatedChildren));
+};
+
+export const getChildren = () => {
+  const children = JSON.parse(localStorage.getItem('children')) || [];
+  return children;
+};
